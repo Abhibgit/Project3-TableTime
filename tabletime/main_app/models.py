a33a88d67150fe0ecf2ac7dc42be54d6bd2616d2
Abhishek Bhatia <abhishekbhatia@lin.lan>
2021-12-11 17:22:58 -0500

    created an in memory model for restaurant to test and it works. chahnges to models.py run migrations

--- tabletime/main_app/models.py
+++ tabletime/main_app/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Restaurant(models.Model):
-     names = models.CharField(max_length = 50)
-     city = models.CharField(max_length = 100)
-     cuisine = models.TextField(max_length = 100)
-     cost = models.IntegerField()
+    name = models.CharField(max_length = 100)
+    city = models.CharField(max_length = 100)
+    cuisine = models.TextField(max_length = 100)
+    cost = models.IntegerField()
 
-     def __str__(self):
-         self.name = name
-         self.city = city
-         self.cuisine = cuisine 
-         self.cost = cost 
+    # def __str__(self):
+    #     self.name = name
+    #     self.city = city
+    #     self.cuisine = cuisine 
+    #     self.cost = cost 
 
 class User(models.Model):
      firstname = models.CharField(max_length = 100)
