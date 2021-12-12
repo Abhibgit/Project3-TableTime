86e4c688e10d7be44048d4f27af5d7d4195507f4
Abhishek Bhatia <abhishekbhatia@lin.lan>
2021-12-12 15:09:46 -0500

    commented out admin auth for user. fixed broken page. Update to models. please pull.

--- tabletime/main_app/views.py
+++ tabletime/main_app/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
@@ -5,22 +6,9 @@ from django.contrib.auth.models import User
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from .models import Restaurant
 
-# Add the Restaurant class & list and view function below the imports
-class Restaurant:
-    def __init__(self, name, city, cuisine, cost,): 
-
-        self.name = name
-        self.city = city
-        self.cuisine = cuisine 
-        self.cost = cost 
-
-restaurant = [ 
-     Restaurant('Burgers & Fries', 'Toronto', 'American', 2),
-     Restaurant('Kobe', 'Toronto', 'Japanese', 3),
-]
-
-     # Note that parens are optional if not inheriting from another class
+# Note that parens are optional if not inheriting from another class
  
 # Create your views here.
 
@@ -28,7 +16,7 @@ def home(request):
   return render(request,'homepage.html')
 
 def restaurant_index(request):
-    restaurant = []
+    restaurant = Restaurant.objects.all() 
     return render(request,'restaurantpage/restaurant.html', {'restaurant': restaurant})
 
 def userprofile_index(request):
@@ -43,7 +31,7 @@ def account_settings(request):
 def savedrestaurants_index(request):
     savedrestaurants=[]
     return render(request, 'savedrestaurants/index.html/', {'savedrestaurants':savedrestaurants})    
-
+    
 def signup(request):
   error_message = ''
   if request.method == 'POST':
@@ -57,4 +45,8 @@ def signup(request):
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
+
+def restaurant_detail(request, restaurant_id):
+    restaurant = restaurant.objects.get(id=restaurant_id)
+    return render(request, 'restaurantpage/restaurant_detail.html', {'restaurant':restaurant})
 
