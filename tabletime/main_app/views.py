1eab13ae98d326ef89d73df6a0ce63a794113c31
Abhishek Bhatia <abhishekbhatia@lin.lan>
2021-12-12 11:44:23 -0500

    added templates for signup, added models and modified views for User and Reviews

--- tabletime/main_app/views.py
+++ tabletime/main_app/views.py
@@ -3,20 +3,8 @@ from django.http import HttpResponse
 from django.views.generic import ListView
 
 # Add the Restaurant class & list and view function below the imports
-class Restaurant:
-    def __init__(self, name, city, cuisine, cost,): 
 
-        self.name = name
-        self.city = city
-        self.cuisine = cuisine 
-        self.cost = cost 
-
-restaurant = [ 
-     Restaurant('Burgers & Fries', 'Toronto', 'American', 2),
-     Restaurant('Kobe', 'Toronto', 'Japanese', 3),
-]
-
-     # Note that parens are optional if not inheriting from another class
+# Note that parens are optional if not inheriting from another class
  
 # Create your views here.
 
@@ -25,7 +13,7 @@ def home(request):
 
 def restaurant_index(request):
     restaurant = []
-    return render(request,'restaurantpage/restaurant.html', {'restaurant': 'restaurant'})
+    return render(request,'restaurantpage/restaurant.html', {'restaurant': restaurant})
 
 def userprofile_index(request):
     return render(request,'userprofile/index.html', {'userprofile':'userprofile'})
@@ -34,10 +22,10 @@ def restaurant_reservation(request):
     return render(request,'reservation.html')
 
 def account_settings(request):
-    return HttpResponse('<h1>Give me your info</h1>')
+    return render(request,'accountsettings/account_settings.html')
 
 def signup(request):
-    return HttpResponse('<h1>Welcome to the matrix</h1>')
+    return render(request,'signup.html')
 
 def login(request):
     return HttpResponse('<h1>So your back huh</h1>')
@@ -46,5 +34,3 @@ def savedrestaurants_index(request):
     savedrestaurants=[]
     return render(request, 'savedrestaurants/index.html/', {'savedrestaurants':savedrestaurants})    
 
-def login(request):
-    return HttpResponse('<h1>So your back huh</h1>')
