2256d99c9c18d35ec35b349fbb315e86ea25a5e8
Abhishek Bhatia <abhishekbhatia@lin.lan>
2021-12-13 22:19:28 -0500

    fixed reviews page and link on restaurant details. completed resturant details opage and success on create reviews for resturant. deleted migrations log. fixed merge conflicts

--- tabletime/main_app/urls.py
+++ tabletime/main_app/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
   path('restaurant_reservation/', views.restaurant_reservation, name='restaurant_reservation'),
   path('account_settings/', views.account_settings, name='account_settings'),
   path('accounts/signup/', views.signup, name='signup'),
-  path('savedrestaurants/', views.savedrestaurants_index, name='savedrestaurants'),
+  path('savedrestaurants/', views.saved_restaurants_index, name='saved_restaurants'),
   path('restaurant/<int:restaurant_id>/', views.restaurant_detail, name='detail'),
   path('restaurant/<int:restaurant_id>/add_review/', views.add_review, name='add_review'),
 ]
